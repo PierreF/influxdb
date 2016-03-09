30a6852ccaddcee3dd0734caa3c01d35df7576a5
Jonathan A. Sternberg <jonathan@influxdb.com>
2016-03-09 09:03:17 -0500

    Allow a query to be killed
    While this allows a query to be killed, it doesn't really do anything
    yet since the interrupt happens only after the first row gets emitted
    (the entire first series).
    
    This section of code will likely have to be refactored to make this work
    since we need a way to interrupt a currently running iterator.

--- cluster/query_executor.go
+++ cluster/query_executor.go
@@ -169,6 +169,8 @@ func (e *QueryExecutor) executeQuery(query *influxql.Query, database string, chu
 			err = e.executeGrantStatement(stmt)
 		case *influxql.GrantAdminStatement:
 			err = e.executeGrantAdminStatement(stmt)
+		case *influxql.KillQueryStatement:
+			err = e.executeKillQueryStatement(stmt)
 		case *influxql.RevokeStatement:
 			err = e.executeRevokeStatement(stmt)
 		case *influxql.RevokeAdminStatement:
@@ -371,6 +373,13 @@ func (e *QueryExecutor) executeGrantStatement(stmt *influxql.GrantStatement) err
 
 func (e *QueryExecutor) executeGrantAdminStatement(stmt *influxql.GrantAdminStatement) error {
 	return e.MetaClient.SetAdminPrivilege(stmt.User, true)
+}
+
+func (e *QueryExecutor) executeKillQueryStatement(stmt *influxql.KillQueryStatement) error {
+	if e.QueryManager == nil {
+		return influxql.ErrNoQueryManager
+	}
+	return e.QueryManager.DetachQuery(stmt.QueryID)
 }
 
 func (e *QueryExecutor) executeRevokeStatement(stmt *influxql.RevokeStatement) error {
